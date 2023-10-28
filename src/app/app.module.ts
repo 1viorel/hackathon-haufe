da9fca88fb6e89198b2f82726043282700df29ae
Viorel <soituviorel@gmail.com>
2023-10-28 11:47:10 +0300

    Upload web

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { TutorialComponent } from './tutorial/tutorial.component';
+import { RuleComponent } from './rule/rule.component';
+import { MapComponent } from './map/map.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TutorialComponent,
+    RuleComponent,
+    MapComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
